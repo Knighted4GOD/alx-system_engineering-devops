3145e3b0ff7eca346a5c46aece7a2ac7ff8ec288
Abraham <Knighted4GOD@gmail.com>
2023-04-25 08:19:16 +0000

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
-"
-rts to-do list information for a given employee ID to CSV format."""
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
